7b94a914d9c594e16bf8239c4615dec68b85721c
Geraklion13 <49410254+Geraklion13@users.noreply.github.com>
2019-04-10 00:20:31 +0300

    ДЗ №2 - дополнение

--- sandbox/src/main/java/MyFirstProgram.java
+++ sandbox/src/main/java/MyFirstProgram.java
@@ -1,11 +1,13 @@
 public class MyFirstProgram {
 
     public static void main(String[] args) {
-        Point p1 = new Point(2, 7);
-        Point p2 = new Point(6, 5);
-        System.out.println("Расстояние между точками = " + distance1(p1, p2));
+        Point p = new Point(7,2, 6,5);
+        System.out.println("Расстояние между точками = " + distance(p));
     }
-    public static double distance1(Point p1, Point p2) {
-        return Math.sqrt(Math.pow((p2.x - p1.x), 2) + Math.pow((p2.y - p1.y), 2));
+
+    public static double distance(Point p) {
+        return Math.sqrt(Math.pow((p.x2 - p.x1), 2) + Math.pow((p.y2 - p.y1), 2));
+
     }
+
 }
--- sandbox/src/main/java/Point.java
+++ sandbox/src/main/java/Point.java
@@ -1,10 +1,28 @@
 public class Point {
 
-    public double x;
-    public double y;
+    public double x1;
+    public double y1;
 
-    public Point(double x, double y) {
-        this.x = x;
-        this.y = y;
+    public double x2;
+    public double y2;
+
+
+
+    public Point(double x1, double y1, double x2, double y2) {
+        // точка p1
+        this.x1 = x1;
+        this.y1 = y1;
+
+        // точка p2
+        this.x1 = x2;
+        this.y1 = y2;
+
+
     }
+
+/*
+    public  double distance() {
+        return Math.sqrt(Math.pow((p2.x - p1.x), 2) + Math.pow((p2.y - p1.y), 2));
+    }
+*/
 }
