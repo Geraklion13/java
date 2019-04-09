0ac7db3371e8f5756c50c744a44b1e8d826005f0
Geraklion13 <49410254+Geraklion13@users.noreply.github.com>
2019-04-09 23:06:17 +0300

    Задание №2 - 1, 2, 3 пункты

--- sandbox/src/main/java/MyFirstProgram.java
+++ sandbox/src/main/java/MyFirstProgram.java
@@ -1,8 +1,13 @@
+
 public class MyFirstProgram {
 
-public static void main(String[] args) {
-System.out.println("Hello world!");
-}
+    public static void main(String[] args) {
+        Point p1 = new Point(2, 7);
+        Point p2 = new Point(6, 5);
+        System.out.println("Расстояние между точками = " + distance1(p1, p2));
+    }
 
-
+    public static double distance1(Point p1, Point p2) {
+        return Math.sqrt(Math.pow((p2.x - p1.x), 2) + Math.pow((p2.y - p1.y), 2));
+    }
 }
