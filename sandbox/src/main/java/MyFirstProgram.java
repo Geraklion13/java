21e2b950e9490cdde2ee3f9d264d544ea62e6ce2
Geraklion13 <49410254+Geraklion13@users.noreply.github.com>
2019-04-10 00:25:00 +0300

    ДЗ 2 пункт 4

--- sandbox/src/main/java/MyFirstProgram.java
+++ sandbox/src/main/java/MyFirstProgram.java
@@ -2,12 +2,6 @@ public class MyFirstProgram {
 
     public static void main(String[] args) {
         Point p = new Point(7,2, 6,5);
-        System.out.println("Расстояние между точками = " + distance(p));
+        System.out.println("Расстояние между точками = " + p.distance());
     }
-
-    public static double distance(Point p) {
-        return Math.sqrt(Math.pow((p.x2 - p.x1), 2) + Math.pow((p.y2 - p.y1), 2));
-
-    }
-
 }
--- sandbox/src/main/java/Point.java
+++ sandbox/src/main/java/Point.java
@@ -2,27 +2,19 @@ public class Point {
 
     public double x1;
     public double y1;
-
     public double x2;
     public double y2;
-
-
 
     public Point(double x1, double y1, double x2, double y2) {
         // точка p1
         this.x1 = x1;
         this.y1 = y1;
-
         // точка p2
         this.x1 = x2;
         this.y1 = y2;
-
-
     }
 
-/*
-    public  double distance() {
-        return Math.sqrt(Math.pow((p2.x - p1.x), 2) + Math.pow((p2.y - p1.y), 2));
+    public double distance() {
+        return Math.sqrt(Math.pow((this.x2 - this.x1), 2) + Math.pow((this.y2 - this.y1), 2));
     }
-*/
 }
