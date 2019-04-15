3f8631d3139550717a37d46a943dc195c61b7491
Geraklion13 <49410254+Geraklion13@users.noreply.github.com>
2019-04-15 18:49:59 +0300

    Домашнее задание №4. Упростил локатор кнопки авторизации

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/SessionHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/SessionHelper.java
@@ -12,7 +12,7 @@ public class SessionHelper extends HelperBase {
     public void login(String username, String password) {
         type(By.name("user"), username);
         type(By.name("pass"), password);
-        click(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Password:'])[1]/following::input[2]"));
+        click(By.cssSelector("input[type='submit']"));
     }
 
 }
