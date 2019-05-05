c4fe25d300bb35d40d85496e57d5e80657bdea53
Geraklion13 <49410254+Geraklion13@users.noreply.github.com>
2019-05-05 17:45:44 +0300

    Лекция 3.10

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/Group/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/Group/GroupDeletionTests.java
@@ -2,6 +2,7 @@ package ru.stqa.pft.addressbook.tests.Group;
 
 
 import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.tests.TestBase;
 
 public class GroupDeletionTests extends TestBase {
@@ -9,6 +10,9 @@ public class GroupDeletionTests extends TestBase {
     @Test(testName = "Удаление существующей группы")
     public void testGroupDeletionTests() {
         app.getNavigationHelper().goToGroupPage();
+        if (! app.getGroupHelper().isThereAGroup()) {
+            app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+        }
         app.getGroupHelper().selectGroup();
         app.getGroupHelper().deleteSelectedGroups();
         app.getGroupHelper().returnToGroupPage();
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/Group/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/Group/GroupModificationTests.java
@@ -9,6 +9,9 @@ public class GroupModificationTests extends TestBase {
     @Test(testName = "Редактирование существующей группы")
     public void testGroupModification() {
         app.getNavigationHelper().goToGroupPage();
+        if (! app.getGroupHelper().isThereAGroup()) {
+            app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+        }
         app.getGroupHelper().selectGroup();
         app.getGroupHelper().initGroupModification();
         app.getGroupHelper().fillGroupForm(new GroupData("test1", "test2343", "test3454"));
